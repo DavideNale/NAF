8100b6098911b9d7f461e2d31d0c9541e863fe58
Davide Nale <davide.nale.99@gmail.com>
2023-08-01 17:06:47 +0200

    new euclidean distance & layers

--- sound_loader.py
+++ sound_loader.py
@@ -16,17 +16,18 @@ class sound_samples(torch.utils.data.Dataset):
         # Calculate mean and standard deviation of the dataset
         self.mean_value = np.mean(self.spectrograms)
         self.std_deviation = np.std(self.spectrograms)
-        
+        self.max_amplitude = np.min(self.spectrograms)
+
         # Calculate min_xy
-        min_x = min(np.min(self.posMic[0]), np.min(self.posSrc[0]))
-        min_y = min(np.min(self.posMic[1]), np.min(self.posSrc[1]))
-        min_z = min(np.min(self.posMic[2]), np.min(self.posSrc[2]))
+        min_x = min(np.min(self.posMic[:,0]), np.min(self.posSrc[:,0]))
+        min_y = min(np.min(self.posMic[:,1]), np.min(self.posSrc[:,1]))
+        min_z = min(np.min(self.posMic[:,2]), np.min(self.posSrc[:,2]))
         self.min_pos = np.array((min_x, min_y, min_z))
 
         # Calculate max_xy
-        max_x = max(np.max(self.posMic[0]), np.max(self.posSrc[0]))
-        max_y = max(np.max(self.posMic[1]), np.max(self.posSrc[1]))
-        max_z = max(np.max(self.posMic[2]), np.max(self.posSrc[2]))
+        max_x = max(np.max(self.posMic[:,0]), np.max(self.posSrc[:,0]))
+        max_y = max(np.max(self.posMic[:,1]), np.max(self.posSrc[:,1]))
+        max_z = max(np.max(self.posMic[:,2]), np.max(self.posSrc[:,2]))
         self.max_pos = np.array((max_x, max_y, max_z))
 
         # Join in a single list of (src, mic, IR) objects
@@ -55,11 +56,37 @@ class sound_samples(torch.utils.data.Dataset):
         freqs = np.random.randint(0, sound_size[0], self.num_samples)
 
         # Normalization
+        freqs_norm = freqs/sound_size[0]
+        times_norm = times/sound_size[1]
+
+        # Ground truths
+        gts = spectrogram[freqs, times]
+
+        return gts, src_norm, mic_norm, freqs_norm, times_norm
+
+    def test(self, idx):
+        # Retrieve source and microphone position
+        s, m = self.indices[idx]
+        src = self.posSrc[s]
+        mic = self.posMic[m]
+
+        # Normalization
+        src_norm = ((src - self.min_pos)/(self.max_pos-self.min_pos) - 0.5) * 2.0
+        mic_norm = ((mic - self.min_pos)/(self.max_pos-self.min_pos) - 0.5) * 2.0
+
+        # Sample <num_samples> frequencies and times from the spectrogram
+        spectrogram = self.spectrograms[s,m]
+        sound_size = spectrogram.shape
+        times = np.random.randint(0, sound_size[1], self.num_samples)
+        freqs = np.random.randint(0, sound_size[0], self.num_samples)
+
+        # Normalization
         times_norm = times/sound_size[1]
         freqs_norm = freqs/sound_size[0]
 
         # Ground truths
         gts = spectrogram[freqs, times]
 
-        return gts, src_norm, mic_norm, freqs_norm, times_norm
+        return gts, src_norm, mic_norm, freqs_norm, times_norm, spectrogram
+        
         
